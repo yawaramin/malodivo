eb5d383b56d0eb9e87305bf4c609c212aad658bc
Yawar Amin <yawar.amin@gmail.com>
2017-06-25 17:34:54 +0600

    Start skeleton allocation function

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
-module Main where
+module Main (main) where
 
 import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BS
@@ -28,38 +28,49 @@ data District =
       districtAvailFund :: Fund } deriving (Generic, Show)
 
 data CategoryFund =
-  Cf
+  CategoryFund
     { cfDistrict :: Id District,
       cfCategory :: Id Category,
       cfAmount :: Fund } deriving (Generic, Show)
 
-data BillSpecificFund =
-  Bsf
-    { bsfDistrict :: Id District,
-      bsfBill :: Id Bill,
-      bsfAmount :: Fund } deriving (Generic, Show)
+data BillFund =
+  BillFund
+    { bfDistrict :: Id District,
+      bfBill :: Id Bill,
+      bfAmount :: Fund } deriving (Generic, Show)
 
 data Input =
   Input
     { inputCategories :: [Category],
       inputBills :: [Bill],
       inputDistricts :: [District],
-      inputCdfs :: [CategoryFund],
-      inputBsfs :: [BillSpecificFund],
+      inputCdfs :: [CategoryFund], -- ^ Category default funds.
+      inputBsfs :: [BillFund], -- ^ Bill-specific funds.
       inputCategoryCaps :: [CategoryFund] } deriving (Generic, Show)
 
-newtype Output =
-  Output { output :: [BillSpecificFund] } deriving (Generic)
+newtype Output = Output { output :: [BillFund] } deriving (Generic)
 
-allocate _ = ()
+districtFund categories bills cdfs bsfs categoryCaps (District { districtId }) =
+  BillFund { bfDistrict = districtId, bfBill = Id 1, bfAmount = 1 }
+
+allocate :: Input -> Output
+allocate Input { inputCategories, inputBills, inputDistricts, inputCdfs, inputBsfs, inputCategoryCaps } =
+  Output
+    { output =
+        map
+          (districtFund inputCategories inputBills inputCdfs inputBsfs inputCategoryCaps)
+          inputDistricts }
 
 instance FromJSON (Id a)
-instance FromJSON BillSpecificFund
+instance ToJSON (Id a)
+instance FromJSON BillFund
+instance ToJSON BillFund
 instance FromJSON CategoryFund
 instance FromJSON District
 instance FromJSON Bill
 instance FromJSON Category
 instance FromJSON Input
+instance ToJSON Output
 
 main :: IO ()
 main = interact $ \input ->
